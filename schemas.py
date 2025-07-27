148bab36908599a499a612b21a5d3d5f75e24de3
mallikarjun <shirwalmallu@gmail.com>
2025-07-27 21:57:30 +0530

    schemas changes

--- schemas.py
+++ schemas.py
@@ -4,10 +4,43 @@ from typing import Optional,List
 
 
 class PreProfile(BaseModel):
-    name: str
-    gender: str
-    religion: str
-    caste: str
+    name: Optional[str] = None
+    surname: Optional[str] = None
+    age: Optional[str] = None
+    height: Optional[str] = None
+    motherTongue:Optional[str] = None
+    maritalStatus: Optional[str] = None
+    eatingHabits: Optional[str] = None
+    religion: Optional[str] = None
+    community: Optional[str] = None
+    subCaste: Optional[str] = None
+    gothra: Optional[str] = None
+    dosha: Optional[str] = None
+    star: Optional[str] = None
+    rassi: Optional[str] = None
+    horoscope: Optional[str] = None
+    profession: Optional[str] = None
+    qualification: Optional[str] = None
+    jobSector: Optional[str] = None
+    income: Optional[str] = None
+    familyStatus: Optional[str] = None
+    familyType: Optional[str] = None
+    fatherOccupation: Optional[str] = None
+    motherOccupation: Optional[str] = None
+    brothers: Optional[str] = None
+    sisters: Optional[str] = None
+    country: Optional[str] = None
+    state: Optional[str] = None
+    city: Optional[str] = None
+    ancestralOrigin: Optional[str] = None
+    mobile: Optional[str] = None
+    socialMedia: Optional[str] = None
+    about: Optional[str] = None
+    image_base64: Optional[str] = None
+       # user_ids who liked this user
+
+
+
 
 class UserRegister(BaseModel):
     username: str
